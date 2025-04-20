0b84a60a762ff07a5bfd8ae367a5d6a58718742c
phancyn <phancyncloud@gmail.com>
2025-04-20 12:46:57 +0500

    Add page BloatWare for unistall UWP and OEM soft

---
+++ PWin11 Tweaker's/Script/BloatwareItem.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PWin11_Tweaker_s.Script
+{
+    public class BloatwareItem : INotifyPropertyChanged
+    {
+        private string _name = "";
+        private string _type = "";
+        private long _size;
+        private string _recommendation = "";
+        private string _packageName = "";
+        private bool _isSelected;
+
+        public string Name
+        {
+            get => _name;
+            set { _name = value; OnPropertyChanged(); }
+        }
+
+        public string Type
+        {
+            get => _type;
+            set { _type = value; OnPropertyChanged(); }
+        }
+
+        public long Size
+        {
+            get => _size;
+            set { _size = value; OnPropertyChanged(); }
+        }
+
+        public string Recommendation
+        {
+            get => _recommendation;
+            set { _recommendation = value; OnPropertyChanged(); }
+        }
+
+        public string PackageName
+        {
+            get => _packageName;
+            set { _packageName = value; OnPropertyChanged(); }
+        }
+
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set { _isSelected = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- PWin11 Tweaker's/obj/x64/Release/net8.0-windows10.0.26100.0/win-x64/PWin11 Tweaker's.AssemblyInfo.cs
+++ PWin11 Tweaker's/obj/x64/Release/net8.0-windows10.0.26100.0/win-x64/PWin11 Tweaker's.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Tweaker For Windows 11")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.10.1")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.10.1+128db7a515cb1a3cd703eadb98e0e887856d66d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.10.1+d04844b225231608a599c48b21c83edf758deb76")]
 [assembly: System.Reflection.AssemblyProductAttribute("PWin11 Tweaker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PWin11 Tweaker\'s")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.10.1")]
